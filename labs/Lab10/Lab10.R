c6e8b80f701c821750712ab43fb1d73f657cead4
vanessawong26 <vanessawong26@users.noreply.github.com>
2020-02-12 19:37:25 -0500

    lab10

--- labs/Lab10/Lab10.R
+++ labs/Lab10/Lab10.R
@@ -40,12 +40,21 @@ data(sat)
 # using a 95% level of significance
 sat1 <- lm(takers ~ expend, data=sat)
 summary(sat1)
+confint(sat1, level=0.95)
+#95% CI for slope of regression of takers on expend is (7.047, 16.228). 0 is not within the interval, therefore there
+#exists some relationship between expend and takers.
+
 
 # What if we control for other factors?
 sat2 <- lm(takers ~ expend + ratio + salary, data=sat)
 summary(sat2)
+sat2
+#When we control for ratio and salary, expenditure has no significant effect on takers (% eligible for SAT).
+#Covariation between new variables to takers and of new variables and expend are very similar to covariation between expend and takers
 
 # How do you interpret the coefficient for salary?
+#Coefficient jumps from + to - and reliable to unreliable after controlling for other variables --> very likely that other variables are doing 
+#covarying with Y in a similar way
 
 
 # What if we include all the variables in the data?

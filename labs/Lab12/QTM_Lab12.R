c06627e21bf34329ff8e506739b3045e8aae7f59
Jeffrey Ziegler <zieglerjef@gmail.com>
2020-02-19 12:10:30 -0500

    Update labs

--- labs/Lab12/QTM_Lab12.R
+++ labs/Lab12/QTM_Lab12.R
@@ -41,55 +41,28 @@ lapply(c("faraway"), pkgTest)
 load("Busby_Football.RData")
 colnames(x)
 
-# Experimental setting: Busby et al. (2017) randomly assigned students 
-# from Ohio State University (OSU) and University of Oregon (UO) to 
-# answer a survey before and after the 2015 College Football Playoff 
-# National Championship game. OSU won the game 42-20, and thus, OSU is 
-# the "winning school" and UO is the "losing school." They find that among 
-# OSU students, presidential approval was higher for those who answered 
-# the survey after the game than those who answered the survey before the 
-# game. By contast, among UO students, presidential approval was lower for
-# those who answered the survey after the game than those who answered the 
-# survey before the game. t-tests below confirm their findings.
 
-# papprove: presidential approval
-# Post: 0 = pre-game survey vs 1 = post-game survey
-# osu: 0 = UO (losing) vs 1 = OSU (winning)
-
-# Comparison of OSU students before and after the game
-t.test(papprove ~ Post, data=x[x$osu==1,])
-
-# Comparison of UO students before and after the game
-t.test(papprove ~ Post, data=x[x$osu==0,])
-
-# Source: Besby, Ethan C, James N. Druckman, and Alexandria Fredendall,
-# 2017, "The Political Relevance of Irrelevant Events," Journal of 
-# Politics 79(1).
-
-
-
-
-# 2. Run a linear model with papprove as a dependent variable and 
+# 1. Run a linear model with papprove as a dependent variable and 
 #    Post, osu, and the interaction of the two as independent variables.
 
 
 
 
-# 3. Answer the following questions based on the results.
+# 2. Answer the following questions based on the results.
 
-# 3a. What is the predicted presidential approval of OSU students who received
+# 2a. What is the predicted presidential approval of OSU students who received
 #     the survey BEFORE the game?
 
 
-# 3b. What is the predicted presidential approval of OSU students who received
+# 2b. What is the predicted presidential approval of OSU students who received
 #     the survey AFTER the game?
 
 
-# 3c. What is the predicted presidential approval of UO students who received
+# 2c. What is the predicted presidential approval of UO students who received
 #     the survey BEFORE the game?
 
 
-# 3d. What is the predicted presidential approval of UO students who received
+# 2d. What is the predicted presidential approval of UO students who received
 #     the survey AFTER the game?
 
 
